48864eaefd209bb047aca25d199cab905e2237ff
Amandeep563 <amandeepbollampalli@gmail.com>
2025-11-10 15:30:42 +0530

    feat(components): add cart and mobile nav * Implement cart content display * Add mobile navigation drawer

--- frontend/src/components/Cart/CartContent.tsx
+++ frontend/src/components/Cart/CartContent.tsx
@@ -1,11 +1,24 @@
+import { Trash } from "lucide-react"
+
+type CartProducts = {
+    productId: number,
+    name: string,
+    size: string,
+    color: string,
+    quantity: number,
+    price: number,
+    image: string
+}
+
+
 const CartContent = () => {
-    const cartProducts = [
+    const cartProducts: CartProducts[] = [
         {
             productId: 1,
             name: "Sourav",
             size: "M",
             color: "Red",
-            quantity: 1,
+            quantity: 3,
             price: 1500,
             image: "https://picsum.photos/200?random=1"
         },
@@ -26,12 +39,51 @@ const CartContent = () => {
             quantity: 1,
             price: 500,
             image: "https://picsum.photos/200?random=1"
+        },
+        {
+            productId: 3,
+            name: "Aman",
+            size: "M",
+            color: "White",
+            quantity: 1,
+            price: 50,
+            image: "https://picsum.photos/200?random=1"
         }
 
     ]
     return (
-        <div>CartContent</div>
-    )
+        <div>
+            {cartProducts.map((product) => (
+                <div
+                    key={product.productId}
+                    className="flex items-start justify-between py-4 border-b"
+                >
+                    <div className="flex items-start">
+                        <img
+                            src={product.image}
+                            alt={product.name}
+                            className="w-20 h-24 object-cover mr-4 rounded"
+                        />
+                        <div>
+                            <h3>{product.name}</h3>
+                            <p className="text-sm text-gray-500">size: {product.size} | color: {product.color}</p>
+                            <div className="flex items-center mt-2">
+                                <button className="border rounded px-2 py-1 text-xl font-medium">-</button>
+                                <span className="mx-4">{product.quantity}</span>
+                                <button className="border rounded px-2 py-1 text-xl font-medium">+</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p className="font-medium">$ {product.price.toLocaleString()}</p>
+                        <button>
+                            <Trash className="h-6 w-6 text-red-600" />
+                        </button>
+                    </div>
+                </div>
+            ))
+            }
+        </div >)
 }
 
 export default CartContent
--- frontend/src/components/Common/Navbar.tsx
+++ frontend/src/components/Common/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Menu, ShoppingCart, UserRound } from "lucide-react";
+import { CircleX, Menu, ShoppingCart, UserRound } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import CartDrawer from "../Layout/CartDrawer";
@@ -6,6 +6,11 @@ import SearchBar from "./SearchBar";
 
 const Navbar = () => {
     const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
+    const [navDrawerOpen, setNavDrawerOpen] = useState<boolean>(false)
+
+    const ToggleNavBar = () => {
+        setNavDrawerOpen(!navDrawerOpen)
+    }
 
     const handleToggle = () => {
         setDrawerOpen(!drawerOpen);
@@ -61,13 +66,30 @@ const Navbar = () => {
                         <SearchBar />
                     </div>
 
-                    <button className="md:hidden">
+                    <button className="md:hidden" onClick={ToggleNavBar}>
                         <Menu className="h-6 w-6 text-gray-700" />
                     </button>
                 </div>
             </nav>
             <CartDrawer drawerOpen={drawerOpen} handleToggle={handleToggle} />
-        </>
+            {/* mobile Nav */}
+            <div
+                className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg z-50 transform transition-transform duration-300
+    ${navDrawerOpen ? "translate-x-0" : "-translate-x-full"}`}
+            >
+                <div className="flex justify-end p-4">
+                    <button onClick={ToggleNavBar}>
+                        <CircleX className="h-6 w-6 text-gray-600" />
+                    </button>
+                </div>
+
+                <div className="flex flex-col space-y-6 p-6">
+                    <Link onClick={ToggleNavBar} className="text-lg font-medium text-gray-700" to="#">Men</Link>
+                    <Link onClick={ToggleNavBar} className="text-lg font-medium text-gray-700" to="#">Women</Link>
+                    <Link onClick={ToggleNavBar} className="text-lg font-medium text-gray-700" to="#">Top Wear</Link>
+                    <Link onClick={ToggleNavBar} className="text-lg font-medium text-gray-700" to="#">Bottom Wear</Link>
+                </div>
+            </div> </>
     );
 };
 
